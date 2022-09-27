873da6d548984e470c07921d64faa540b6b344e8
im-so-tired <markov-pt@mail.ru>
2022-09-27 19:58:30 +0300

    com(RootActions):add comment about union form actions

--- client/app/Store/RootActions.ts
+++ client/app/Store/RootActions.ts
@@ -1,10 +1,10 @@
-import {mainFormActions} from './Slices/MainForm/MainFormSlice'
+import { mainFormActions } from './Slices/MainForm/MainFormSlice'
 import * as userActions from './Slices/User/User.action'
 
 export const allUserActions = {
 	...userActions,
 }
-
+// union form actions
 export const allFormActions = {
-	...mainFormActions
+	...mainFormActions,
 }
